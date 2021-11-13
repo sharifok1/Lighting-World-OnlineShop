6b684f3eb8490fb9da33508dae25f59becc634fc
sharifok1 <sharifok1@gmail.com>
2021-11-13 19:45:06 +0600

    done

--- src/Pages/DashBoard/DashboardHome/DashboardHome.js
+++ src/Pages/DashBoard/DashboardHome/DashboardHome.js
@@ -3,7 +3,12 @@ import React from 'react';
 const DashboardHome = () => {
     return (
         <div>
-            <h1>wel come to deshboard</h1>
+            <h3>
+                The only and one decoration lighting shop . <br />
+                Where you can find world classes product.
+                <img style={{width:'100%'}} src="https://i.ibb.co/vsb5qHf/e.jpg" alt="" />
+            </h3>
+           <img src="" alt="" />
         </div>
     );
 };
